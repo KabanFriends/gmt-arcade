4d090e6b57161d731650cc92701b053735c60f3d
KabanFriends <yuukun2004@protonmail.ch>
2024-02-10 18:06:58 +0900

    initial

---
+++ js/play.js
@@ -0,0 +1,21 @@
+let player;
+
+function start(name) {
+    let swfobject = {};
+    
+    swfobject.embedSWF = function(url, cont) {
+        let ruffle = window.RufflePlayer.newest();
+        
+        player = Object.assign(document.getElementById(cont).appendChild(ruffle.createPlayer()));
+
+        player.config = {
+            letterbox: "on",
+            autoplay: "on",
+            unmuteOverlay: "hidden",
+        };
+
+        player.load({ url: url });
+    }
+
+    swfobject.embedSWF(`/swf/${name}.swf`, 'ruffle');
+}
